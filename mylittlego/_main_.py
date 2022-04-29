7177b7e492ac3dcd7bf54fd0123562582310d25f
neojou <neojou@gmail.com>
2022-04-29 18:16:11 +0800

    mylittlego: screen theme style
    Signed-off-by: neojou <neojou@gmail.com>

--- mylittlego/_main_.py
+++ mylittlego/_main_.py
@@ -3,7 +3,11 @@ import os
 import kivy
 kivy.require("2.0.0")
 
+from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
 from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
 
 import sys
 sys.path.append("../")
@@ -12,16 +16,25 @@ from mylittlego.core.constants import (
     VERSION,
 )
 
-class KaTrainApp(MDApp):
+class MyLittleGo(MDApp):
     def __init__(self):
         super().__init__()
 
     def build(self):
         self.title = f"{PROGRAM} v{VERSION}"
+        self.theme_cls.theme_style = "Dark"
+        screen = MDScreen()
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Hello, World",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        )
+        return screen
 
 
 def run_app():
-    app = KaTrainApp()
+    app = MyLittleGo()
     app.run()
 
 if __name__ == "__main__":
